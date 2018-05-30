ea50be8361e40c79bb6510d1059273c5bb05654e
wang yun <wang.yun@nyspi.columbia.edu>
2018-05-30 16:36:22 -0400

    adding roi to roi

--- alex_freesurfer_new.sh
+++ alex_freesurfer_new.sh
@@ -5,10 +5,13 @@ cd /Volumes/Pegasus_wangyun/ADHD/ITA/MRI/fmripre_outputs/freesurfer
 # /Volumes/Pegasus_wangyun/ADHD/ITA/MRI/Preprocess
 #
 #  find  ./*/Structural  -mindepth 1 -maxdepth 1 -type d -exec rsync  -rR {}  /Volumes/Pegasus_wangyun/ADHD/ITA/MRI/fmripre_outputs/freesurfer  \;
-find  ./sub*/*/   -mindepth 1 -maxdepth 1 -type d > file_need_move.txt
+find  ./sub*/*   -mindepth 1 -maxdepth 1 -type d > file_need_move.txt
 
 cat "file_need_move.txt" | while read C; do
         sub=`echo $C | cut -d'/' -f2`
-        #mkdir sub-$sub
-        mv -n  $C  $sub
+        echo $sub
+        newfolder=`echo $sub/*.nii  | cut -d'/' -f2 | cut -d'.' -f1`
+        mkdir $newfolder
+        mv -n  $C  $newfolder
+        mv -n $sub/*.nii  $newfolder
 done
